4cc6bc890157656c304eb392b68cdab47057efb5
Ni Dang <47034704+Dawn0916@users.noreply.github.com>
2024-09-08 12:32:14 +0000

    update main.py and model.py

--- app/main.py
+++ app/main.py
@@ -1,18 +1,25 @@
 from pydantic import BaseModel # this has nothing to do my machine learning models
 # this is just the parent class for everything that is strictly typed in Pydantic
+
 from fastapi import FastAPI, Depends, UploadFile, File
+
 # we need this to upload images to fastAPI
 # this is the Python image library
 from PIL import Image
+
 # This is what we use the BaseModel for
-# the result is strictly typed so that it returns
+# the result is strictly typed so that it returns 
 # a string for the category (label that we predict)
 # and a float for the confidence (the probability for the label)
 class Result(BaseModel):
     category: str
     confidence: float
-# this creates an instance for the endpoint
+
+
+# this creates an instance for the endpoint 
 app = FastAPI()
+
+
 @app.post('/predict', response_model=Result )
 async def predict(
     input_image: UploadFile = File(...),
--- app/model.py
+++ app/model.py
@@ -4,19 +4,26 @@ import torch
 from torch import nn
 from torchvision import transforms
 from torchvision.models import resnet18, ResNet
-#todo: remember to delete the use of the
-# loadotenv and os.getenv entries
+
+#todo: remember to delete the use of the 
+# loadotenv and os.getenv entries 
 # when we use the Docker image later
 from loadotenv import load_env
+
 load_env(file_loc='/workspaces/fruit-classifier-endpoint/app/.env')
+
 MODELS_DIR = 'models'
 MODEL_FILE_NAME = 'model.pth'
-CATEGORIES = ["freshapples", "freshbanana", "freshoranges",
+
+CATEGORIES = ["freshapples", "freshbanana", "freshoranges", 
               "rottenapples", "rottenbanana", "rottenoranges"]
-#todo: delete
+
+#todo: delete 
 #print(os.getenv('WANDB_API_KEY'))
+
 def download_artifact():
     assert 'WANDB_API_KEY' in os.environ, 'Please enter WANDB_API_KEY as an environmental variable.'
+
     wandb.login() # here we get access to the artifact registry
     # go to your artifact registry to find this full path
     # antonios-org/banana_apple_orange/resnet18:v1
@@ -24,29 +31,44 @@ def download_artifact():
     wandb_project = os.environ.get('WANDB_PROJECT')
     wandb_model_name = os.environ.get('WANDB_MODEL_NAME')
     wandb_model_version = os.environ.get('WANDB_MODEL_VERSION')
+
     #here we reconstruct the artifact path
     artifact_path = f'{wandb_org}/{wandb_project}/{wandb_model_name}:{wandb_model_version}'
     artifact = wandb.Api().artifact(artifact_path, type='model')
     artifact.download(root=MODELS_DIR)
 
-def get_raw_model()->ResNet:
+
+def get_raw_model() -> ResNet:
+
     N_CLASSES = 6
 
     model = resnet18(weights=None)
 
-    # check that this archetecture is the same in your Kaggle notebook
+    # check that this architecture is the same in your Kaggle notebook
     model.fc = nn.Sequential(
-        nn.Linear(512,512)
+        nn.Linear(512, 512),
         nn.ReLU(),
         nn.Linear(512, N_CLASSES)
     )
-    return model
 
-def load_model()->ResNet:
+    return model 
+
+def load_model() -> ResNet:
     download_artifact()
-
     model = get_raw_model()
     model_state_dict_path = os.path.join(MODELS_DIR, MODEL_FILE_NAME)
-    model_state_dict = torch.load(model_state_dict_path, map_location = 'cpu')
-    model.load_state_dict(model_state_dict, strict = True)
+    model_state_dict = torch.load(model_state_dict_path, map_location='cpu')
+    # this was strict=False in the code here https://github.com/aihpi/fruit-classifier-gcloud-run/blob/main/app/model.py
+    model.load_state_dict(model_state_dict, strict=True) 
     model.eval()
+
+    return model
+
+def load_transforms() -> transforms.Compose:
+    return transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406],
+                             [0.229, 0.224, 0.225])
+    ])
